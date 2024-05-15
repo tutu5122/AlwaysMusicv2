af7dada2ad14d39f4eaa1031c4c9b7df6456defe
tutu512 <mjperez5122@gmail.com>
2024-05-15 00:55:33 -0400

    segundo commit

--- db/conexion.js
+++ db/conexion.js
@@ -9,10 +9,22 @@ const pool = new Pool({
     
     user: 'postgres',
     host: 'localhost',
-    database: 'music',
+    database: 'music', 
     password: '1234',
     port: 5432
 
  });
 
 export default pool;
+
+
+// la base de dato que cree para el desfio 
+// CREATE DATABASE music;
+
+// CREATE TABLE estudiantes (
+//     id SERIAL PRIMARY KEY,
+//     Nombre VARCHAR(100),
+//     Rut VARCHAR(20),
+//     Curso VARCHAR(50),
+//     Nivel VARCHAR(50)
+// );
